4009d6ffee6073ca5e579002666906f2edfb50df
elienn2000 <elias.ennajimi@gmail.com>
2025-10-06 21:32:20 +0200

    Topbar design edit and added buttons for navigation

--- PFSpa/portfolioSpa/src/app/topbar/topbar.component.ts
+++ PFSpa/portfolioSpa/src/app/topbar/topbar.component.ts
@@ -2,24 +2,32 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateService } from '@ngx-translate/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-topbar',
   standalone: true,
-  imports: [CommonModule, TranslateModule],
+  imports: [MatButtonModule, CommonModule, TranslateModule, MatToolbarModule, MatSelectModule, MatFormFieldModule],
   templateUrl: './topbar.component.html',
   styleUrls: ['./topbar.component.scss']
 })
 export class TopbarComponent {
+
+  selLang = 'en';
+  selectedMenu = 'home';
+
   constructor(private translate: TranslateService) {
     // Aggiungo le lingue disponibili
     translate.addLangs(['it', 'en']);
     translate.setFallbackLang('it');
-    translate.use('it'); // lingua di default
+    this.switchLang();
   }
 
   // Cambio lingua quando clicco sulla bandiera
-  switchLang(lang: string) {
-    this.translate.use(lang);
+  switchLang() {
+    this.translate.use(this.selLang);
   }
 }
