d798725f39ddb7f4f752c3c77b72a0608e9bc2d3
elienn2000 <elias.ennajimi@gmail.com>
2025-10-09 07:21:41 +0200

    Added a form module and handled parameters for login and registry forms

--- PFSpa/portfolioSpa/src/app/pages/login/login.component.ts
+++ PFSpa/portfolioSpa/src/app/pages/login/login.component.ts
@@ -1,36 +1,75 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, Validators } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 
 import { AuthService } from '../../services/auth.service';
 
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule, TranslateModule, MatCardModule, MatTabsModule, MatInputModule],
+  imports: [CommonModule, FormsModule, TranslateModule, MatCardModule, MatTabsModule, MatInputModule, MatFormFieldModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  email = '';
-  password = '';
+  // email = '';
+  // password = '';
 
-  constructor(private authService: AuthService) {}
+  loginForm: FormGroup;
+  registerForm: FormGroup;
+
+  constructor(private authService: AuthService, private fb: FormBuilder) {
+
+
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+
+    this.registerForm = this.fb.group({
+      username: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required]
+    });
+
+
+  }
 
   onLogin() {
-    // Per ora solo console.log, più avanti gestirai i token
-    this.authService.login({ email: this.email, password: this.password }).subscribe({
-      next: (response) => {
-        console.log('Login successful:', response);
-      },
-      error: (error) => {
-        console.error('Login failed:', error);
-      }
-    });
+    if (this.loginForm.valid) {
+      // Per ora solo console.log, più avanti gestirai i token
+      this.authService.login({ email: this.loginForm.get('email')?.value, password: this.loginForm.get('password')?.value }).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        }
+      });
+    }
+  }
+
+  onRegister() {
+    if (this.registerForm.valid) {
+      // Per ora solo console.log, più avanti gestirai i token
+      this.authService.login({ email: this.registerForm.get('email')?.value, password: this.loginForm.get('password')?.value }).subscribe({
+        next: (response) => {
+          console.log('Login successful:', response);
+        },
+        error: (error) => {
+          console.error('Login failed:', error);
+        }
+      });
+    }
   }
 }
